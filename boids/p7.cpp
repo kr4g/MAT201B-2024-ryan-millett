b673773bc0857a15a5ff5360c7e34a98f780a5be
Ryan <rmillett@ucsb.edu>
2024-03-12 12:49:36 -0700

    typo

--- boids/p7.cpp
+++ boids/p7.cpp
@@ -13,7 +13,7 @@
 #include "al_ext/statedistribution/al_CuttleboneStateSimulationDomain.hpp"
 
 #include "../utils/octree.cpp"
-// #include "classes/boid_3.cpp"
+// #include "classes/boid_4.cpp"
 
 const int CUBE_SIZE = 25;
 
@@ -113,7 +113,6 @@ struct MyApp : DistributedAppWithState<CommonState> {
 
 
     // place the camera so that we can see the axes
-    // nav().pos(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE * 1.167);
     initDist = al::dist(nav().pos(), Vec3d(0, 0, 0));
     // nav().pos(CUBE_SIZE, CUBE_SIZE * 0.5, CUBE_SIZE * 1.5);
     nav().pos(Vec3f(0.0));
