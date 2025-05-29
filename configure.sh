d725bd47804141005641fc939bdbecf15967de38
Kon Hyong Kim <konhyong@gmail.com>
2025-05-29 13:41:40 -0700

    updated chmod for script files and turned off jack

--- configure.sh
+++ configure.sh
@@ -4,4 +4,4 @@ mkdir -p build
 cd build
 mkdir -p release
 cd release
-cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
+cmake -DRTAUDIO_API_JACK=OFF -DRTMIDI_API_JACK=OFF -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
